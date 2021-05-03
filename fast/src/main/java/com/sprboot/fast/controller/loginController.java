7da94ec3fb4658b3b04668718f6c264f34c8421e
wx1114 <1261008090@qq.com>
2021-05-03 17:46:16 +0800

    diyiban

--- fast/src/main/java/com/sprboot/fast/controller/loginController.java
+++ fast/src/main/java/com/sprboot/fast/controller/loginController.java
@@ -1,10 +1,11 @@
-package controller;
+package com.sprboot.fast.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Controller
 public class loginController {
     @RequestMapping("/gin")
     public String hello (){
